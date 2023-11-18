993f85e11ac2ea55a5c0b8ea9e60c20962314330
letiendat040904@gmail.com <letiendat040904@gmail.com>
2023-11-18 08:40:00 +0700

    #2  finished feature

--- index.php
+++ index.php
@@ -34,22 +34,10 @@
                     if ($user) {
                         if ($user['password'] == $password) {
                             $MESSAGE = "Đăng nhập thành công!";
-                            // if (exist_param("remember")) {
-                            //     add_cookie("user_name", $user_name, 30);
-                            //     add_cookie("password", $password, 30);
-                            // } else {
-                            //     delete_cookie("user_name");
-                            //     delete_cookie("password");
-                            // }
                             $_SESSION["user"] = $user;
                             $MESSAGE = "Đăng nhập thành thành công!";
-                            if (isset($_SESSION['request_uri'])) {
-                               // header("location: " .$_SESSION['request_uri']);
-                            }
-                            // ob_start();
-                            // header("location: project_polyfood/index.php");
-                            // ob_end_flush();
-                            // die;
+                            echo "<meta http-equiv='refresh' content='0;URL=?url=trangchu'/>";
+
                         } else {
                             $error['password'] = "Thông tin mật khẩu không chính xác!";
                         }
@@ -162,25 +150,55 @@
                 break;
 
             case 'capnhattk':
-                
+                check_login();
+                extract($_REQUEST);
+
+                if (exist_param("btn_update")) {
+                    $error = [];
+                    $name = trim($name);
+                    $phone = trim($phone);
+                    $flag = true;
+                    if (strlen($name) < 6) {
+                        $error['name'] = "Họ và tên phải có ít nhất 6 ký tự!";
+                        $flag = false;
+                    }
+                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        $error['email'] = "Email không đúng định dạng!";
+                        $flag = false;
+                    }
+                    if (users_exist_by_email_id($email, $user_id)) {
+                        $error['email'] = "Email này đã được sử dụng!";
+                        $flag = false;
+                    }
+                    if (strlen($phone) != 10 || !is_numeric($phone)) {
+                        $error['phone'] = "Vui lòng nhập số điện thoại!";
+                        $flag = false;
+                    }
+                    if (!preg_match('/((09|03|07|08|05)+([0-9]{8})\b)/', $phone)) {
+                        $error['phone'] = "Số điện thoại không đúng!";
+                        $flag = false;
+                    }
+                    if (users_exist_by_phone_id($phone, $user_id)) {
+                        $error['phone'] = "Số điện thoại này đã được sử dụng!";
+                        $flag = false;
+                    }
+                    if ($flag) {
+                        $image_upload = save_file("image_upload", "$IMAGE_DIR/users/");
+                        $image = strlen("$image_upload") > 0 ? $image_upload : $image;
+                        try {
+                            update_users($user_name, $password, $name, $email, $phone, $image, $role_id, $user_id);
+                            $MESSAGE = "Cập nhật thành công!";
+                            $_SESSION['user'] = select_by_name_users($user_name);
+                        } catch (Exception $exc) {
+                            $MESSAGE = "Cập nhật thất bại!";
+                        }
+                    }
+                } else {
+                    extract($_SESSION['user']);
+                }
                 include_once"src/components/account/capnhattk.php";
                 break;
             
-            // case 'product':
-            //     extract($_REQUEST);
-            //         $cates = categories_select_all();
-            //         if (exist_param("category_id")) {
-            //             $items = products_select_by_categories($category_id);
-            //         } else if (exist_param("search_product")) {
-            //             $items = products_select_keyword($keyword);
-            //         } else {
-            //             $items = products_select_all();
-            //         }
-            //         include_once"view/client/product.php";
-            //     break;
-
-
-            // default:
             require_once"view/client/home.php";
                 break;
         }
--- src/components/account/capnhattk.php
+++ src/components/account/capnhattk.php
@@ -1,7 +1,7 @@
 <main class="w-full mt-14">
         <h1 class="text-3xl text-center mt-10 mb-10">Cài đặt tài khoản</h1>
         <div class="form__login my-10 w-full flex justify-center mb-20">
-          <form action="update-account.php" method="post" enctype="multipart/form-data" class="form__login--content w-[900px] sm:w-[900px]  flex flex-col  gap-7">
+          <form action="?url=capnhattk" method="post" enctype="multipart/form-data" class="form__login--content w-[900px] sm:w-[900px]  flex flex-col  gap-7">
             <?php
             if (strlen($MESSAGE)) {
               echo "<h5 class=''>$MESSAGE</h5>";
@@ -12,7 +12,7 @@
                             <div class="form__group flex flex-col gap-2 mb-6">
                         <label for="user_name">Tên đăng nhập</label>
                         <input type="text" name="user_name" id="username" class="form__input  text-xs border border-gray-700 p-3 w-full rounded-lg
-                            focus:border-orange-500 focus:outline-none" placeholder="Tên đăng nhập" readonly value="<?= $user_name ?>" />
+                            focus:border-orange-500 focus:outline-none" placeholder="Tên đăng nhập"  value="<?= $user_name ?>" />
                         <span class="text-red-500 text-xs">
                             <?php echo isset($error['user_name']) ? $error['user_name'] : ''; ?>
                         </span>
@@ -54,7 +54,7 @@
                     
                     <label for="image_upload">Avatar</label>
                     <input type="file" name="image_upload" id="image_upload" class="form__input  text-xs border border-gray-700  p-3 w-full rounded-lg focus:border-orange-500 focus:outline-none" placeholder="image" />
-                    <img class="" style="width:150px;height:150px;display:block;margin:0 auto;" src="<?= $CONTENT_URL ?>/images/users/<?= $image ?>">
+                    <img class="" style="width:150px;height:150px;display:block;margin:0 auto;" src="<?= $IMAGE_DIR ?>users/<?= $image ?>">
                     </div>
                 </div>
                 
@@ -63,9 +63,6 @@
             <div class="form__group flex  justify-center items-center gap-3">
                     <button type="submit" name="btn_update" class="text-white bg-orange-600 p-2 mb-6 rounded-lg w-full sm:w-[100px] text-center">
                         UPDATE
-                    </button>
-                    <button type="reset" name="btn_reset" class="text-white bg-orange-600 p-2 mb-6 rounded-lg w-full sm:w-[100px] text-center">
-                        RESET
                     </button>
                     </div>
             <div>
